2055f667dd22b0ec57b0681c7a42e3d2fdb84d1e
Enoch Root <enochroot@openmailbox.org>
2018-10-10 00:57:44 -0300

    [Mod] Metodo getCurso

--- app/controlador.js
+++ app/controlador.js
@@ -1,12 +1,15 @@
 var Curso = require('./models/curso');
 
 exports.getCurso = function(req, res) {
+    // CORS
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
     Curso.find(function(err, curso) {
-        if (err) {
-            res.send(err)
-        }        
-        res.json(curso) 
-    });
+        if (err) res.send(err);    
+        
+        res.json(curso);
+    });    
 }
 
 exports.setCurso = function(req, res) {
